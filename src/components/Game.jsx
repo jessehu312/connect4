a72a707ba9abb94a3fb74f367303dfa1a74a0bc0
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-01-10 03:33:07 -0800

    Update Logo, Multiplayer State (#6)

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Board from './Board';
 import Status from './Status';
 import Roster from './Roster';
@@ -21,15 +21,23 @@ const Game = (_) => {
     setBoard(newBoard);
 
     const result = checkState(newBoard);
-    if (result) {
+    if (!result || gameState === GAME_TYPE.SINGLE) {
       setGameState(result);
     }
 
-    setPlayer(currentPlayer === PLAYER.ONE ? PLAYER.TWO : PLAYER.ONE);
+    if (window.sendMatch && gameType === GAME_TYPE.MULTIPLAYER) {
+      console.log('!!!Send Game State');
+      console.log(newBoard)
+      if (result === GAME_STATE.ONGOING)
+        setGameState(GAME_STATE.HOLD);
+      window.sendMatch(newBoard);
+    } else {
+      setPlayer(currentPlayer === PLAYER.ONE ? PLAYER.TWO : PLAYER.ONE);
+    }
   };
 
   const reset = () => {
-    if (gameType !== GAME_TYPE.SINGLE && gameState === GAME_STATE.ONGOING) {
+    if (gameType !== GAME_TYPE.SINGLE && (gameState === GAME_STATE.ONGOING || gameState === GAME_STATE.HOLD)) {
       return;
     }
     setBoard(EMPTY_BOARD);
@@ -37,7 +45,45 @@ const Game = (_) => {
     setGameState(GAME_STATE.ONGOING);
   };
 
-  console.log(`${gameType} - ${playerName}`);
+  const onMatch = data => {
+    const result = checkState(data);
+    setGameState(result);
+    console.log(`!!! Received Game State - CURRENT_STATE: ${gameState} - HOLD: ${GAME_STATE.HOLD}`);
+    console.log(data);
+    setBoard(data);
+  };
+
+  const onAccept = (data, socket) => {
+      reset();
+      // YOU ARE PLAYER 1, Send Match
+      console.log('!!! Received Accept')
+      window.sendMatch = ((newBoard) => {
+        if (!newBoard) {
+          console.log('null');
+          return;
+        }
+        socket.emit('match', { ...data, "board": newBoard});
+      });
+  };
+
+  const onNewMatch = ({players, eventEmitter}) => {
+    reset();
+    setGameState(GAME_STATE.HOLD);
+    setPlayer(PLAYER.TWO);
+    const matchId = Date.now();
+    // YOU ARE PLAYER 2, Send Accept
+    console.log('!!! Send Accept')
+    window.sendMatch = ((newBoard) => {
+      if (!newBoard) {
+        console.log('null');
+        return;
+      }
+      eventEmitter.socket.emit('match', { id: matchId, players, "board": newBoard});
+    })
+    eventEmitter.match({ id: matchId, players, "board": EMPTY_BOARD });
+  };
+
+  //console.log(`${gameType} - ${playerName}`);
   return (
   <Box className="Game" display="flex">
     <Container>
@@ -45,7 +91,7 @@ const Game = (_) => {
       <Container>
         <FormGroup row>
           <Button variant="contained" color="primary" onClick={reset}>Reset</Button>
-          <div style={{width: "30px"}}/>
+          <div style={{width: "20px"}}/>
           <FormControlLabel
             control={
               <Switch
@@ -64,7 +110,9 @@ const Game = (_) => {
       </Container>
       <Board {...{ board, gameState, currentPlayer, onCellClick}}/>
     </Container>
-    {(gameType === GAME_TYPE.MULTIPLAYER && playerName) ? <EventProvider {...{playerName, gameType}}><Roster/></EventProvider>: null}
+    {(gameType === GAME_TYPE.MULTIPLAYER && playerName) ? 
+      <EventProvider {...{playerName, gameType, onMatch, onAccept}}><Roster {...{onNewMatch}}/></EventProvider>: null
+    }
   </Box>
   )
 
